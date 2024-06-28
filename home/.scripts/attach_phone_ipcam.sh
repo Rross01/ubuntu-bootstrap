e7dc7aed5854e5d31cf724ce70829c50fb40d74e
Roman Rossamakhin <rossamakhin01@gmail.com>
2024-06-28 21:33:44 +0500

    improve ip webcam script

--- home/.scripts/attach_phone_ipcam.sh
+++ home/.scripts/attach_phone_ipcam.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo -n "Enter IP: "
-read -r IP
 sudo modprobe -r v4l2loopback
 sudo modprobe v4l2loopback video_nr=9 card_label=Video-Loopback exclusive_caps=1
-ffmpeg -i http://$IP:8080/video -vf format=yuv420p -f v4l2 /dev/video9
+ffmpeg -i http://$1:8080/video -vf format=yuv420p -f v4l2 /dev/video9
