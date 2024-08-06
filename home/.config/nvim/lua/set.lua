05f92f0ef28ea5631bd106fa92b5866cb9a02b51
Roman Rossamakhin <rossamakhin01@gmail.com>
2024-08-06 13:27:14 +0500

    format nvim confs

--- home/.config/nvim/lua/set.lua
+++ home/.config/nvim/lua/set.lua
@@ -11,9 +11,9 @@ vim.opt.cursorline = true
 
 vim.opt.termguicolors = true
 
-vim.opt.number = true     -- Включаем нумерацию строк
-vim.opt.so = 999          -- Отступ сверху и снизу при скролинге
-vim.opt.undofile = true   -- Возможность отката назад
+vim.opt.number = true -- Включаем нумерацию строк
+vim.opt.so = 999 -- Отступ сверху и снизу при скролинге
+vim.opt.undofile = true -- Возможность отката назад
 vim.opt.splitright = true -- vertical split вправо
 vim.opt.splitbelow = true -- horizontal split вниз
 
@@ -35,17 +35,17 @@ vim.opt.incsearch = true
 -- Табы и отступы
 vim.opt.syntax = "ON"
 vim.opt.smartindent = true -- autoindent new lines
-vim.opt.expandtab = true   -- use spaces instead of tabs
-vim.opt.shiftwidth = 4     -- shift 4 spaces when tab
-vim.opt.tabstop = 4        -- 1 tab == 4 spaces
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- shift 4 spaces when tab
+vim.opt.tabstop = 4 -- 1 tab == 4 spaces
 
 -- Подсвечивает на доли секунды скопированную часть текста
 vim.api.nvim_exec(
-    [[
+	[[
 augroup YankHighlight
 autocmd!
 autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=300}
 augroup end
 ]],
-    false
+	false
 )
