fd1399efe5ca842b523bf402c7c84eded7f64c91
Roman Rossamakhin <rossamakhin01@gmail.com>
2024-08-20 20:10:11 +0500

    refactor tmux session wizard

--- home/.scripts/tmux_session_wizard.sh
+++ home/.scripts/tmux_session_wizard.sh
@@ -1,49 +1,50 @@
 #!/bin/bash
 
-FZF_OPTS="--bind=alt-k:up,alt-j:down"
+FZF_OPTS="--bind=alt-k:up,alt-j:down --layout=reverse"
 
-function tmux_session_wizard() {
-    PLACES_TO_FZF="
-        $HOME/ \
-        $HOME/.config \
-        $HOME/ownCloud \
-        $HOME/temp \
-        $HOME/places/sys \
-        $HOME/places/apps \
-        $HOME/places/personal \
-        $HOME/places/work \
-        $HOME/places/personal/git \
-        $HOME/places/work/git"
+DIRS_TO_FIND="\
+$HOME \
+$HOME/.config \
+$HOME/ownCloud \
+$HOME/temp \
+$HOME/places/sys \
+$HOME/places/personal \
+$HOME/places/work \
+$HOME/places/personal/git \
+$HOME/places/work/git"
 
-    RESULT=$( (tmux list-sessions -F "#{session_name}: #{session_windows} window(s)\
-        #{?session_grouped, (group ,}\
-        #{session_group}#{?session_grouped,),}\
-        #{?session_attached, (attached),}";
-        find -L $PLACES_TO_FZF -mindepth 1 -maxdepth 1 -type d) | fzf $FZF_OPTS --layout=reverse | tail -n 1)
+TMUX_LIST_FORMAT="\
+#{session_name}: #{session_windows} window(s)\
+#{?session_grouped, (group ,}\
+#{session_group}#{?session_grouped,),}\
+#{?session_attached, (attached),}\
+"
 
-    if [ -z "$RESULT" ]; then
-        exit 0
+TMUX_SESSIONS=$(tmux list-sessions -F "$TMUX_LIST_FORMAT")
+DIRS_LIST=$(find $DIRS_TO_FIND -mindepth 1 -maxdepth 1 -type d)
+
+RESULT=$((echo "$TMUX_SESSIONS"; echo "$DIRS_LIST") | fzf $FZF_OPTS | tail -n 1)
+
+if [ -z "$RESULT" ]; then
+    exit 0
+fi
+
+# Get or create session
+if [[ $RESULT == *":"* ]]; then
+    # RESULT comes from list-sessions
+    SESSION=$(echo $RESULT | awk '{print $1}')
+    SESSION=${SESSION//:/}
+else
+    # RESULT is a path
+    SESSION=$(basename "$RESULT" | tr . - | tr ' ' - | tr ':' - | tr '[:upper:]' '[:lower:]')
+    if ! tmux has-session -t=$SESSION 2> /dev/null; then
+        tmux new-session -d -s $SESSION -c "$RESULT"
     fi
+fi
 
-    # Get or create session
-    if [[ $RESULT == *":"* ]]; then
-        # RESULT comes from list-sessions
-        SESSION=$(echo $RESULT | awk '{print $1}')
-        SESSION=${SESSION//:/}
-    else
-        # RESULT is a path
-        SESSION=$(basename "$RESULT" | tr . - | tr ' ' - | tr ':' - | tr '[:upper:]' '[:lower:]')
-        if ! tmux has-session -t=$SESSION 2> /dev/null; then
-            tmux new-session -d -s $SESSION -c "$RESULT"
-        fi
-    fi
-
-    # Attach to session
-    if [ -z "$TMUX" ]; then
-        tmux attach -t $SESSION
-    else
-        tmux switch-client -t $SESSION
-    fi
-}
-
-tmux_session_wizard
+# Attach to session
+if [ -z "$TMUX" ]; then
+    tmux attach -t $SESSION
+else
+    tmux switch-client -t $SESSION
+fi
