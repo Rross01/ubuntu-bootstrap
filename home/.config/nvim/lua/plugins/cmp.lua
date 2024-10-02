ffc6c6d5c02225358b4bdd26e14407f88b322b36
Roman Rossamakhin <rossamakhin01@gmail.com>
2024-10-02 14:11:19 +0500

    nvim small refactor

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -1,11 +1,11 @@
 return {
 	{
-		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
 		dependencies = {
 			{ "hrsh7th/cmp-buffer" },
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "hrsh7th/cmp-path" },
-			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-cmdline" },
 		},
 		config = function()
 			local cmp = require("cmp")
