a1dc83b1a02eeab2fd37a35ce906b1e028032e22
Roman Rossamakhin <rossamakhin01@gmail.com>
2024-10-24 13:07:59 +0500

    try zen neovim plugin

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -11,6 +11,15 @@ return {
 	{ "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
 
 	{
+		"folke/zen-mode.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
 		opts = {
