ab23b104c887f81efc856666067edaf17536bf80
Roman Rossamakhin <rossamakhin01@gmail.com>
2024-11-08 17:44:46 +0500

    nvim refactor

--- home/.config/nvim/lua/plugins/other.lua
+++ home/.config/nvim/lua/plugins/other.lua
@@ -1,4 +1,33 @@
 return {
+	{ "lewis6991/gitsigns.nvim", config = true },
+	{ "windwp/nvim-autopairs", config = true },
+	{ "brenoprata10/nvim-highlight-colors", config = true },
+	{ "tpope/vim-fugitive" },
+	{ "ThePrimeagen/harpoon", branch = "harpoon2", dependencies = { "nvim-lua/plenary.nvim" } },
+	{ "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
+	{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" } },
+	{ "nvim-pack/nvim-spectre" },
+
+	{
+		'navarasu/onedark.nvim',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme onedark")
+		end,
+	},
+
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			"s1n7ax/nvim-window-picker",
+		},
+	},
+
 	{
 		"robitx/gp.nvim",
 		config = function()
