5b69f4d46c51a554daed5e04db61f0879e3dddb4
Roman Rossamakhin <rossamakhin01@gmail.com>
2024-12-12 11:55:09 +0500

    nvim: theming

--- home/.config/nvim/lua/plugins/other.lua
+++ home/.config/nvim/lua/plugins/other.lua
@@ -21,7 +21,7 @@ return {
 		priority = 1000,
 		config = function()
 			vim.g.gruvbox_material_background = "medium"
-			vim.g.gruvbox_material_foreground = "original"
+			vim.g.gruvbox_material_foreground = "mix"
 			-- vim.g.gruvbox_material_transparent_background = 2
 			vim.cmd.colorscheme("gruvbox-material")
 		end,
